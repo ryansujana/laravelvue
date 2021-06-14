99f3931d7e8988242c398f47633561411edbcce5
ryansujana <ryan.sujana25@gmail.com>
2021-06-14 22:14:26 +0700

    Project laravel dengan vue

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<main>
+		<Header />
+		
+		<Sidebar />
+	</main>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
+export default {
+	components: {
+		Header,
+		Sidebar
+	}
+}
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+const app = new Vue({
+	el: '#app',
+	render: h => h(App)
+});
